c76b701957fb3f460f849f525b4d4288dd061ee5
Belmouk <el.mehdi.boukhiam@gmail.com>
2025-10-26 21:47:20 +0100

    Complete exercise 9 in js

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    
+    if (( num1 < 0 || num2 < 0 || Math.floor(num1) !== num1 || Math.floor(num2) !== num2 )) {
+        return "ERROR";
+    } else {
+        const max = num1 > num2 ? num1 : num2;
+        const min = num1 + num2 - max;
+        const arrayToSum = [];
+        for (let i = min; i <= max; i++) {
+            arrayToSum.push(i);
+        }
+        return arrayToSum.reduce( (sum, num) => sum +num, 0);
+    }
+    
 };
 
 // Do not edit below this line
