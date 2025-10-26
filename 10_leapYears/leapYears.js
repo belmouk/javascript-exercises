1f3416fdeb80d1f246e5e1440b627a9ba48fa183
Belmouk <el.mehdi.boukhiam@gmail.com>
2025-10-26 21:59:25 +0100

    Complete exercise 10 in js

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,4 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (year < 0 || !Number.isInteger(year)) {
+        return "ERROR"
+    } else {
+        if (year % 100 === 0) {
+            return year % 400 === 0 ? true : false;
+        } else {
+            return year % 4 === 0 ? true : false;
+        }
+    }
 
 };
 
