6e137d6075059d447c82c2c0e781e386662a4319
Belmouk <el.mehdi.boukhiam@gmail.com>
2025-10-30 14:43:12 +0100

    Complete exercise 13 in js

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (sentence) {
+    const punctuation = { " ": null, ",": null, "." : null, ":" : null, "!" : null, ";" : null, "?" : null};
 
+    const words = sentence.split("").filter(word => !(word in punctuation)).map(word => word.toLowerCase());
+    const wordsReversed = words.toReversed();
+    
+    for (let i =0; i < words.length; i++) {
+        if (words[i] !== wordsReversed[i]) {
+            return false;   
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
