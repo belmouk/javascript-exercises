eae35213f4eb5259fe770b3002c7d528f844fa38
Belmouk <el.mehdi.boukhiam@gmail.com>
2025-10-30 15:22:45 +0100

    Compelte exercise 14 in js

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = Number(num);
 
+    if (isNaN(num) || num < 0){
+        return "OOPS";
+    } else if (num === 0) {
+        return 0;
+    } else if (num === 1) {
+        return 1;
+    } else {
+        const sequence = [1, 1];
+
+        for (let i = 1; i < (num - 1); i++) {
+            const fibonacciTerm = sequence[i] + sequence[i-1];
+            sequence.push(fibonacciTerm);
+        }
+        return sequence.at(-1);
+    }
 };
 
 // Do not edit below this line
