60f195d0dcc51ed7ba70a3ab7da0059d7c0bef10
Belmouk <el.mehdi.boukhiam@gmail.com>
2025-10-30 15:45:31 +0100

    Complete exercise 16 in js

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const sortedArr = arr.toSorted( (a, b) => {
+        const aLife = a.yearOfDeath? (a.yearOfDeath - a.yearOfBirth) : (2025 - a.yearOfBirth);
+        const bLife = b.yearOfDeath? (b.yearOfDeath - b.yearOfBirth) : (2025 - b.yearOfBirth);
 
+        return bLife - aLife;
+    });
+    return sortedArr.at(0);
 };
 
 // Do not edit below this line
